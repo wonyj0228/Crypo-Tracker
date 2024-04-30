8fe282c0e0c1141261ddb5a034be6b4b71b4e492
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-04-30 22:14:41 +0900

    theme 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Container>
+        <H1>안녕</H1>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
