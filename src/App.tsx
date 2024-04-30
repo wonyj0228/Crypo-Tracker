21d5c453484699234da54d053bbd117d7d60f35d
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-04-30 23:32:55 +0900

    reset css, 기초 css 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,79 @@
 import { RouterProvider } from 'react-router-dom';
 import Router from './Router';
+import { createGlobalStyle } from 'styled-components';
+
+// reset css를 사용하고 싶을 때
+// styled component는 컴포넌트에 직접적으로 style을 줌으로, styled component에게 reset 내용을 전달해줄 필요가 있음
+// Global style로 먼저 설정을 해주면 모든 문서의 style component에 적용됨
+const GlobalStyle = createGlobalStyle`
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+/* Custom */
+*{
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+}
+a{
+  text-decoration: none;
+}
+`;
 
 function App() {
-  return <RouterProvider router={Router()} />;
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={Router()} />
+    </>
+  );
 }
 
 export default App;
