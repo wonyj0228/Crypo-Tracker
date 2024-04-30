971c67706589221fdee0e37f3f304389d074376f
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-04-30 22:37:59 +0900

    react router v6 경로설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import { RouterProvider } from 'react-router-dom';
+import Router from './Router';
 
 function App() {
-  return (
-    <div>
-      <Container>
-        <H1>안녕</H1>
-      </Container>
-    </div>
-  );
+  return <RouterProvider router={Router()} />;
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Coins from './routes/Coins';
+import Coin from './routes/Coin';
+
+function Router() {
+  return createBrowserRouter([
+    { path: '/', element: <Coins /> },
+    { path: '/:coinId', element: <Coin /> },
+  ]);
+}
+export default Router;
